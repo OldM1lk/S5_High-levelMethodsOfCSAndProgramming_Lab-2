690f6d8ce36f0a574c5a075bbc6101b9abf74ed3
circumstancesl <gsrmakill@mail.ru>
2025-11-24 19:32:18 +0700

    fix

--- scripts/unitTest.sh
+++ scripts/unitTest.sh
@@ -6,6 +6,6 @@ bash scripts/build.sh
 cd "$(dirname "$0")/.." || exit
 
 java -jar lib/junit-platform-console-standalone.jar \
-  --class-path app.jar \
+  --class-path out/app.jar \
   --scan-class-path \
   --include-package tests
