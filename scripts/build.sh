74fef82883a9e4895d37e60a2db64ca18c2521ab
circumstancesl <gsrmakill@mail.ru>
2025-11-24 19:28:41 +0700

    fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,6 @@ cd "$(dirname "$0")/.."
 rm -rf out
 mkdir -p out
 
-# Определяем разделитель classpath
 if [[ "$OS" == "Windows_NT" ]]; then
     SEP=";"
 else
@@ -19,4 +18,5 @@ fi
 CP="lib/junit-platform-console-standalone.jar${SEP}lib/kotlinx-cli-jvm-0.3.6.jar${SEP}lib/h2-2.4.240.jar"
 
 kotlinc $(find src -name "*.kt") -cp "$CP" -include-runtime -d "out/app.jar"
+
 echo "Сборка завершена: out/app.jar"
--- scripts/run.sh
+++ scripts/run.sh
@@ -6,5 +6,11 @@ bash scripts/build.sh
 # Переход в корень проекта
 cd "$(dirname "$0")/.." || exit
 
+if [[ "$OS" == "Windows_NT" ]]; then
+    SEP=";"
+else
+    SEP=":"
+fi
+
 # Проброс аргументов в программу
-java -cp "out/app.jar;lib/kotlinx-cli-jvm-0.3.6.jar;lib/h2-2.4.240.jar" presentation/MainKt "$@"
+java -cp "out/app.jar${SEP}lib/kotlinx-cli-jvm-0.3.6.jar${SEP}lib/h2-2.4.240.jar" presentation/MainKt "$@"
